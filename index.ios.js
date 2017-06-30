b28aa3eaeb7cd38f3d369c32cc6636c9f4510155
Yvonne Hayes <yvonne.hayes@skyward.io>
2017-06-30 15:58:31 -0700

    started adding logic for ajax call

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,17 @@
 
 // Import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a Component
 const App = () => {
   return (
-    <Header headerText={'Albums'} />
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 };
 
